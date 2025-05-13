68e1737ecbca21d0b72321c827eb8083611edf75
Vaishali Rawat <rawatvaishali254@gmail.com>
2025-05-13 23:21:09 +0530

    print number from 1 -> N

--- Recursion/Beginner/printNumber.js
+++ Recursion/Beginner/printNumber.js
@@ -1,11 +1,7 @@
-// 1 -> n
-// 1 is my current number and n is the last number
-
-function printNumber(current, n){
-    if(current > n) return; //Base case
+function printNumber(current,n){
+    if(current > n) return;
 
     console.log(current);
-    printNumber(current+1, n); //Recursive call
-};
-console.log(printNumber(1,10));
-console.log(printNumber(8,4));
+    printNumber(current+1, n);
+}
+console.log(printNumber(1,6));
