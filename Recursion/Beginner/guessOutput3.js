88545ea4928ec4926eb9458c87ecd836bf14c661
Vaishali Rawat <112968518+Vaishalirawat1119@users.noreply.github.com>
2025-05-14 23:56:10 +0530

    Delete Recursion/Beginner directory

--- Recursion/Beginner/guessOutput3.js
+++
@@ -1,5 +0,0 @@
-function fun(n){
-    if(n === 1) return 0;
-    else return 1 + fun(n/2);
-};
-console.log(fun(16));
--- Recursion/Beginner/printNumber.js
+++
@@ -1,9 +0,0 @@
-// 1 -> N
-
-function printNumber(current, n){
-    if(current > n) return;
-    console.log(current);
-    printNumber(current + 1, n);
-};
-console.log(printNumber(1,7));
-console.log(printNumber(10,6));
