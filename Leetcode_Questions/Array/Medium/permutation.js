a2157d25420d7de8412e50549fc3415348e5c759
Vaishali Rawat <rawatvaishali254@gmail.com>
2025-05-23 22:07:33 +0530

    Next Permutation

---
+++ Leetcode_Questions/Array/Medium/permutation.js
@@ -0,0 +1,27 @@
+function permutation(nums){
+    let i = nums.length-2;
+
+    while(i >= 0 && nums[i] >= nums[i + 1]){
+        i--;
+    }
+
+    if(i >= 0){
+        let j = nums.length-1;
+        while(nums[j] <= nums[i]){
+            j--;
+        }
+        [nums[i], nums[j]] = [nums[j], nums[i]];
+    }
+
+    let left = i + 1;
+    let right = nums.length - 1;
+    while(left < right){
+        [nums[left], nums[right]] = [nums[right], nums[left]];
+        left++;
+        right--;
+    }
+    return nums;
+}
+console.log(permutation([1,2,3]));
+console.log(permutation([3,2,1]));
+console.log(permutation([1,1,5]));
