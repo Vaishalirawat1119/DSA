12519b5a416db30dc80c7bccfc6e22055ad81cd4
Vaishali <rawatvaishali254@gmail.com>
2025-07-02 15:56:30 +0530

    function which returns the second largest number in an array

---
+++ NamasteyDSA/WarmUp/SecondLargest.js
@@ -0,0 +1,7 @@
+// Write a program which retruns the second largest number from the array.
+// Corner case:  What if, array is empty? has only one element? has duplicates?
+function secondLargest(arr){
+    
+}
+
+let arr = [1,5,87,2,8,4,8];
