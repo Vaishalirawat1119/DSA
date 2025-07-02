368dece27699b6c546b00e55f3e159c99248673b
Vaishali <rawatvaishali254@gmail.com>
2025-07-02 23:34:24 +0530

    commit

--- NamasteyDSA/WarmUp/SecondLargest.js
+++ NamasteyDSA/WarmUp/SecondLargest.js
@@ -1,7 +1,22 @@
 // Write a program which retruns the second largest number from the array.
-// Corner case:  What if, array is empty? has only one element? has duplicates?
-function secondLargest(arr){
-    
-}
+// Corner case:  What if, array is empty? has only one element? has duplicates? 
 
-let arr = [1,5,87,2,8,4,8];
+function secondlargestNumber(arr){
+    if(arr.length < 2){
+        return null;
+    }
+    let largest = -Infinity;
+    let second = -Infinity;
+    for(let i=0; i<arr.length; i++){
+        if(arr[i] > largest){
+            second = largest;
+            largest = arr[i];
+        }
+        else if(arr[i] > second && arr[i] !== largest){
+            second = arr[i];
+        }
+    }
+    return second;
+}
+let arr = [3,5,1,2,7,5];
+console.log(secondlargestNumber(arr));
