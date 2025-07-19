6e5530fca806c1888341a86d316e138a35fdc02e
Vaishali <rawatvaishali254@gmail.com>
2025-07-19 22:14:08 +0530

    linear search

---
+++ NamasteyDSA/Search-Sort/Linear_Search.js
@@ -0,0 +1,8 @@
+function linearSearch(arr, target){
+    for(let i=0; i<arr.length; i++){
+        if(arr[i] === target) return i;
+    }
+    return "not in an array";
+}
+let arr = [3,2,5,1,6,8];
+console.log(linearSearch(arr, 3));
