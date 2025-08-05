54540dd1a537509fc0e4c6df1bd39165b835fcce
Vaishali <rawatvaishali254@gmail.com>
2025-08-05 23:04:06 +0530

    valid palindrome

--- NamasteyDSA/String/validPalindrome.js
+++ NamasteyDSA/String/validPalindrome.js
@@ -5,6 +5,12 @@ function palindrome(s){
     //another way to do reverse
     let rev = "";
     for(let i=0; i<s.length; i++){
+        //rejex exp alternate
+        // if(s[i].charCodeAt() >= "a".charCodeAt() && s[i].charCodeAt() <= "z".charCodeAt() || s[i].charCodeAt() >= "0".charCodeAt() && s[i].charCodeAt() <= "9".charCodeAt()){
+        //     filter += s[i];
+        //     rev = s[i] + rev;
+        // }
+        
         if(s[i].match(/[a-z0-9]/i)){
             filter += s[i];
             rev = s[i] + rev;
@@ -13,5 +19,6 @@ function palindrome(s){
     // let rev = filter.split("").reverse().join("");
     return filter === rev;
 }
-let s = "A man, a plan, a canal: Panama";
+// let s = "A man, a plan, a canal: Panama";
+let s = "race a car"
 console.log(palindrome(s));
