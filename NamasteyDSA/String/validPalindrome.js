df24474631c1900d3cbed6f3871d9d3d879be0e3
Vaishali <rawatvaishali254@gmail.com>
2025-08-05 22:53:14 +0530

    another way to do reverse in string

--- NamasteyDSA/String/validPalindrome.js
+++ NamasteyDSA/String/validPalindrome.js
@@ -2,12 +2,15 @@
 function palindrome(s){
     s = s.toLowerCase();
     let filter = "";
+    //another way to do reverse
+    let rev = "";
     for(let i=0; i<s.length; i++){
         if(s[i].match(/[a-z0-9]/i)){
             filter += s[i];
+            rev = s[i] + rev;
         }
     }
-    let rev = filter.split("").reverse().join("");
+    // let rev = filter.split("").reverse().join("");
     return filter === rev;
 }
 let s = "A man, a plan, a canal: Panama";
