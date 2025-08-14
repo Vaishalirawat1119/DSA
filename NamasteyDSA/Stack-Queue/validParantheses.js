173d2a189b0c43f41888d3bcf6fc09cb3920bf6e
Vaishali <rawatvaishali254@gmail.com>
2025-08-14 22:26:31 +0530

    optimise the code

--- NamasteyDSA/Stack-Queue/validParantheses.js
+++ NamasteyDSA/Stack-Queue/validParantheses.js
@@ -1,14 +1,24 @@
 function isValid(s){
     let stack = [];
 
+    let map = {
+        "{" : "}",
+        "[" : "]",
+        "(" : ")"
+    }
+
     for(let i=0; i<s.length; i++){
-        if(s[i] === "{" || s[i] === "(" || s[i] === "["){
+        // if(s[i] === "{" || s[i] === "(" || s[i] === "["){
+        
+        if(map[s[i]]){
             stack.push(s[i]);
         }
         else{
             let top = stack.pop();
 
-            if(!top || (top === "(" && s[i] !== ")") || (top === "{" && s[i] !== "}") || (top === "[" && s[i] !== "]")){
+            // if(!top || (top === "(" && s[i] !== ")") || (top === "{" && s[i] !== "}") || (top === "[" && s[i] !== "]")){
+
+            if(!top || s[i] !== map[top]){
                 return false;
             }
         }
@@ -17,7 +27,7 @@ function isValid(s){
 }
 
 // let s = "{}([])";
-let s = "{()[]([]){([])}}"
+// let s = "{()[]([]){([])}}"
 // let s = "([{}]";
-// let s = "([{})]";
+let s = "([{})]";
 console.log(isValid(s));
