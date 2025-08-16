f5bd5a4189702c16689d7685ace51b57931604d3
Vaishali <rawatvaishali254@gmail.com>
2025-08-16 22:33:35 +0530

    optimise the code

--- NamasteyDSA/Stack-Queue/removeOutermostParantheses.js
+++ NamasteyDSA/Stack-Queue/removeOutermostParantheses.js
@@ -5,19 +5,17 @@ function removeOuterParantheses(x){
     for(let i=0; i<x.length; i++){
         if(x[i] === "("){
             stack.push(x[i])
-            if(stack.length > 1){
-                ans += x[i];
-            }
+                ans += ((stack.length > 1) ? x[i] : "");
         }
         else{
-            if(stack.length > 1){
-                ans += x[i];
-            }
+            ans += ((stack.length > 1) ? x[i] : "");
             stack.pop();
         }
     }
     return ans;
 }
 
-let x = "(()())(())(()(()))"
+// let x = "(()())(())(()(()))"
+let x = "(()())(())"
+// let x = "()()"
 console.log(removeOuterParantheses(x));
