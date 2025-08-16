c8d321b9c1f8d558aa8c1516a5ce0e2158f5bfd9
Vaishali <rawatvaishali254@gmail.com>
2025-08-16 22:46:16 +0530

    remove the outer parantheses without using stack

---
+++ NamasteyDSA/Stack-Queue/removeOutermostParantheses2.js
@@ -0,0 +1,21 @@
+//Approach 2
+
+function removeOuterParantheses(s){
+    let level = 0;
+    let ans = "";
+
+    for(let i=0; i<s.length; i++){
+        if(s[i] === "("){
+            ++level;
+            ans += ((level > 1) ? s[i] : "");
+        }
+        else{
+            ans += ((level > 1) ? s[i] : "");
+            --level;
+        }
+    }
+    return ans;
+}
+
+let s = "(()())(())(()(()))";
+console.log(removeOuterParantheses(s));
