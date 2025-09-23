35e284965c099bb98a73ffd11683678012c90557
Vaishali <rawatvaishali254@gmail.com>
2025-09-23 22:41:45 +0530

    binary tree level order traversal

---
+++ NamasteyDSA/BinaryTree/levelorderTraversal.js
@@ -0,0 +1,33 @@
+function TreeNode(val, left=null, right=null){
+    this.val = val;
+    this.left = left;
+    this.right = right;
+}
+
+function levelOrderTraversal(root){
+    if(!root) return [];
+    let q = [root];
+    let ans = [];
+    while(q.length){
+        let levelArr = [];
+        let levelSize = q.length;
+        for(let i=0; i<levelSize; i++){
+            let curr = q.shift();
+            curr.left && q.push(curr.left);
+            curr.right && q.push(curr.right);
+            levelArr.push(curr.val);
+        }
+        ans.push(levelArr);
+    }
+    return ans;
+}
+
+let root = new TreeNode(3,
+    new TreeNode(9),
+    new TreeNode(20,
+        new TreeNode(15),
+        new TreeNode(7)
+    )
+);
+
+console.log(levelOrderTraversal(root));
