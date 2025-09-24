7b017dc5e0a8e88391cfd0abd7b3dcf702e9ffb5
Vaishali <rawatvaishali254@gmail.com>
2025-09-24 22:05:57 +0530

    binary tree level order traversal using recursion

---
+++ NamasteyDSA/BinaryTree/levelorderTraversal2.js
@@ -0,0 +1,31 @@
+function TreeNode(val, left=null, right=null){
+    this.val = val;
+    this.left = left;
+    this.right = right;
+}
+
+function levelOrderTraversal(root){
+    if(!root) return [];
+    let ans = [];
+    let traversal = (curr, level) => {
+        if(!ans[level]) ans[level] = [];
+        ans[level].push(curr.val);
+        curr.left && traversal(curr.left, level+1);
+        curr.right && traversal(curr.right, level+1);
+    }
+    traversal(root, 0);
+    return ans;
+}
+
+let root = new TreeNode(3,
+    new TreeNode(9,
+        new TreeNode(5),
+        new TreeNode(8),
+    ),
+    new TreeNode(20,
+        new TreeNode(15),
+        new TreeNode(7)
+    )
+);
+
+console.log(levelOrderTraversal(root));
