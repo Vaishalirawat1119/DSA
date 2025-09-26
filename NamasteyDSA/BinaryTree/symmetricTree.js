7221addcad60d4df28f28bff51142ad60dd5daac
Vaishali <rawatvaishali254@gmail.com>
2025-09-26 22:33:58 +0530

    symmetric tree - mirror of itself

---
+++ NamasteyDSA/BinaryTree/symmetricTree.js
@@ -0,0 +1,34 @@
+function TreeNode(val, left=null, right=null){
+    this.val = val;
+    this.left = left;
+    this.right = right;
+}
+
+function symmetricTree(root){
+    let isMirror = (left, right) => {
+        if(!left && !right) return true;
+        if(!left || !right) return false;
+        return left.val === right.val &&
+            isMirror(left.left, right.right) &&
+            isMirror(left.right, right.left);
+    }
+    return isMirror(root.left, root.right);
+}
+
+let root = new TreeNode(1,
+    new TreeNode(2,
+        new TreeNode(3,
+            new TreeNode(5),
+        ),
+        new TreeNode(4)
+    ),
+    new TreeNode(2,
+        new TreeNode(4),
+        new TreeNode(3,
+            null,
+            new TreeNode(5)
+        )
+    )
+);
+
+console.log(symmetricTree(root));
