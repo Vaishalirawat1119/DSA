efea8cc67c9d9dc689da3c468e9cfff114388136
Vaishali <rawatvaishali254@gmail.com>
2025-09-26 22:09:18 +0530

    path sum using bottom up approach - recursion

---
+++ NamasteyDSA/BinaryTree/pathSum2.js
@@ -0,0 +1,39 @@
+function TreeNode(val, left=null, right=null){
+    this.val = val;
+    this.left = left;
+    this.right = right;
+}
+
+function pathSum(root, target){
+    if(!root) return false;
+    if(!root.left && !root.right){
+        return root.val === target;
+    }
+    let leftSubTreeHasPathSum = pathSum(root.left, target - root.val);
+    let rightSubTreeHasPathSum = pathSum(root.right, target - root.val);
+    return leftSubTreeHasPathSum || rightSubTreeHasPathSum;
+}
+
+// let root = new TreeNode(5,
+//     new TreeNode(4,
+//         new TreeNode(11,
+//             new TreeNode(7),
+//             new TreeNode(2)
+//         )
+//     ),
+//     new TreeNode(8,
+//         new TreeNode(13),
+//         new TreeNode(4,
+//             new TreeNode(1),
+//         )
+//     )
+// )
+
+let root = new TreeNode(1,
+    new TreeNode(2),
+    new TreeNode(3)
+);
+
+console.log(pathSum(root, 5));
+
+// console.log(pathSum(root, 22));
