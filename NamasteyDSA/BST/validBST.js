5ce36f59ac994e5558e43e7fa663403763f320a0
Vaishali <rawatvaishali254@gmail.com>
2025-10-03 22:23:22 +0530

    New Module : Binary Search Tree (BST) - Given the root of a binary tree, determine if it is a valid binary search tree (BST).

---
+++ NamasteyDSA/BST/validBST.js
@@ -0,0 +1,43 @@
+function TreeNode(val, left=null, right=null){
+    this.val = val;
+    this.left = left;
+    this.right = right;
+}
+
+function validBST(root){
+    let ans = null;
+    let isBST = (curr, lo, hi) => {
+        if(!curr) return true;
+        if((lo !== null && curr.val <= lo) || (hi !== null && curr.val >= hi)) return false;
+        let isLeftBST = isBST(curr.left, lo, curr.val);
+        let isRightBST = isBST(curr.right, curr.val, hi);
+        return isLeftBST && isRightBST;
+    }
+    ans = isBST(root, null, null);
+    return ans;
+}
+
+let root = new TreeNode(50,
+    new TreeNode(30,
+        new TreeNode(20,
+            new TreeNode(10),
+            new TreeNode(25)
+        ),
+        TreeNode(40,
+            new TreeNode(35),
+            new TreeNode(45)
+        )
+    ),
+    new TreeNode(70,
+        new TreeNode(60,
+            new TreeNode(55),
+            new TreeNode(65)
+        ),
+        new TreeNode(80,
+            new TreeNode(75),
+            new TreeNode(85)
+        )
+    )
+);
+
+console.log(validBST(root));
