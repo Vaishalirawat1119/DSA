c813bba78c994448ebfda9dd6af657ed4674cd44
Vaishali <rawatvaishali254@gmail.com>
2025-11-17 22:21:10 +0530

    Implement climbStairs using bottom-up DP approach

---
+++ NamasteyDSA/DynamicProgramming/climbingStairs.js
@@ -0,0 +1,12 @@
+function climbStairs(n){
+    let dp = [0,1,2];
+    for(let i=3; i<=n; i++){
+        dp[i] = dp[i-1] + dp[i-2];
+    }
+    return dp[n];
+}
+
+console.log(climbStairs(3));
+console.log(climbStairs(7));
+console.log(climbStairs(2));
+console.log(climbStairs(5));
