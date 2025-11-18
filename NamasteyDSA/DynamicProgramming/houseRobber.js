47103711d1e58ce3ac395e752082a74f12fac6cf
Vaishali <rawatvaishali254@gmail.com>
2025-11-18 21:50:48 +0530

    reduces space complexity from O(n) to constant O(1)

--- NamasteyDSA/DynamicProgramming/houseRobber.js
+++ NamasteyDSA/DynamicProgramming/houseRobber.js
@@ -1,11 +1,18 @@
 function houseRobber(arr){
     let n = arr.length;
     if(n === 1) return arr[0];
-    let dp = [arr[0], Math.max(arr[0], arr[1])];
-    for(let i=2; i<n; i++){
-        dp[i] = Math.max(arr[i] + dp[i-2], dp[i-1]);
+    // let dp = [arr[0], Math.max(arr[0], arr[1])];
+    let p1 = p2 = 0;
+    for(let i=0; i<n; i++){
+        // dp[i] = Math.max(arr[i] + dp[i-2], dp[i-1]);
+        let curr = Math.max(arr[i] + p2, p1);
+        let temp = p1;
+        p1 = curr;
+        p2 = temp;
+        curr++
     }
-    return dp[n-1];
+    // return dp[n-1];
+    return p1;
 }
 console.log(houseRobber([1,2,3,1]));
 console.log(houseRobber([2,7,9,3,1]));
