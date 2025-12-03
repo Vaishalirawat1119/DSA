1eccd409c85effa4c19807f798a3c14bf5d97366
Vaishali <rawatvaishali254@gmail.com>
2025-12-03 21:58:58 +0530

    compute max product using left/right cumulative products with zero resets

---
+++ NamasteyDSA/DynamicProgramming/maximumProductSubarray2.js
@@ -0,0 +1,21 @@
+function maxProduct(arr){
+    let n = arr.length;
+    let leftToRight = rightToLeft = 1;
+    let totalMax = -Infinity;
+    for(let i=0; i<n; i++){
+        leftToRight = leftToRight * arr[i];
+        totalMax = Math.max(totalMax, leftToRight);
+        if(leftToRight === 0) leftToRight = 1;
+    }
+    for(let i=n-1; i>=0; i--){
+        rightToLeft = rightToLeft * arr[i];
+        totalMax = Math.max(totalMax, rightToLeft);
+        if(rightToLeft === 0) rightToLeft = 1;
+    }
+    return totalMax;
+}
+
+console.log(maxProduct([2,3,-2,4]));
+console.log(maxProduct([-2,0,-1]));
+console.log(maxProduct([2,-5,3,1,-4,0,-10,2,8]));
+console.log(maxProduct([2,-5,3,1,-4,0,-10,2,-8]));
