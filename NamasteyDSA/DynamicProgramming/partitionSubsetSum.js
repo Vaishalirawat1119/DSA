9886a471a60531d89fe5d171cea0bd4982754477
Vaishali <rawatvaishali254@gmail.com>
2025-12-08 23:55:02 +0530

    add DFS + memo approach to determine if array can be partitioned into equal-sum subsets

---
+++ NamasteyDSA/DynamicProgramming/partitionSubsetSum.js
@@ -0,0 +1,20 @@
+function partitionSum(arr){
+    let sum = arr.reduce((acc, curr) => acc + curr, 0);
+    if(sum % 2) return false;
+    sum = sum / 2;
+    let dp = Array.from({length : sum + 1}, () => Array(arr.length).fill(undefined));
+    let fn = (remS, start) => {
+        if(remS === 0) return true;
+        if(remS < 0) return false;
+        if(dp[remS][start] !== undefined) return dp[remS][start];
+        for(let i=start; i<arr.length; i++){
+            if(fn(remS - arr[i], i+1)){
+                return dp[remS][start] = true;
+            }
+        }
+        return dp[remS][start] = false;
+    }
+    return fn(sum, 0)
+}
+console.log(partitionSum([1,5,11,5]));
+console.log(partitionSum([1,2,3,5]));
