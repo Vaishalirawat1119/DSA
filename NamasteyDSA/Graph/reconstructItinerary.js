fe8c7f7cf325aa36bd3fd0e43c1175fe9cc84da0
Vaishali <rawatvaishali254@gmail.com>
2025-12-23 21:10:10 +0530

    feat: implement DFS-based itinerary reconstruction with lexical ordering

---
+++ NamasteyDSA/Graph/reconstructItinerary.js
@@ -0,0 +1,23 @@
+function findItinerary(tickets){
+    let graph = {};
+    for(let [from, to] of tickets){
+        if(!graph[from]) graph[from] = [];
+        graph[from].push(to);
+    }
+    for(let node in graph){
+        graph[node].sort();
+    }
+    let path = [];
+    let dfs = (curr) => {
+        let destinations = graph[curr] || [];
+        while(destinations.length){
+            let neighbor = destinations.shift();
+            dfs(neighbor);
+        }
+        path.push(curr);
+    }
+    dfs("JFK");
+    return path.reverse();
+}
+console.log(findItinerary(tickets = [["MUC","LHR"],["JFK","MUC"],["SFO","SJC"],["LHR","SFO"]]));
+console.log(findItinerary(tickets = [["JFK","SFO"],["JFK","ATL"],["SFO","ATL"],["ATL","JFK"],["ATL","SFO"]]));
